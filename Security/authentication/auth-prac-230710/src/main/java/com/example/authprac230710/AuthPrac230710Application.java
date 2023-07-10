18335164e54a35f4d893a70f194b4360aa00054b
Yoon Jun Pil <galaxy160102@gmail.com>
2023-07-10 16:36:25 +0900

    Security add

---
+++ Security/authentication/auth-prac-230710/src/main/java/com/example/authprac230710/AuthPrac230710Application.java
@@ -0,0 +1,13 @@
+package com.example.authprac230710;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPrac230710Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthPrac230710Application.class, args);
+    }
+
+}
---
+++ Security/authentication/auth-prac-230710/src/test/java/com/example/authprac230710/AuthPrac230710ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authprac230710;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPrac230710ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
